1475ccec78f9f969fd16eb639ff2f764277795aa
varsha-llm <varsha.t@blubridge.com>
2026-02-11 15:13:36 +0530

    iterator

--- PracticeCode/src/main.rs
+++ PracticeCode/src/main.rs
@@ -1,3 +1,10 @@
+
+
 fn main() {
-    println!("Hello, world!");
+    //println!("Hello, world!");
+    //Iterating over the array
+    let iterator=[1,2,3,4,5,6,7];
+    for i in iterator.iter(){
+        println!("{i}");
+    }
 }
